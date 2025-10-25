6e29dd5f6bb10d959e2981d2d24272134bcd72e7
ahmedmoselhi <ahmedmoselhi55@gmail.com>
2025-10-25 12:05:56 +0300

    intial

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import setup_translate
+
+
+setup(name='enigma2-plugin-extensions-opdboot',
+		version='3.1',
+		author='formiano/ahmedmoselhi',
+		author_email='ahmedmoselhi55@gmail.com',
+		package_dir={'Extensions.OPDBoot': 'src'},
+		packages=['Extensions.OPDBoot'],
+		package_data={'Extensions.OPDBoot': ['*.mvi', '*.png', 'images/*.png']},
+		description='OPD MultiBoot',
+		cmdclass=setup_translate.cmdclass)
