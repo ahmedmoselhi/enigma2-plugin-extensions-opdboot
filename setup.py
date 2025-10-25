5ea882d01c2d559f6f1b8d8da107823400dea0dd
ahmedmoselhi <ahmedmoselhi55@gmail.com>
2025-10-25 13:57:07 +0300

    fix last commit

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='enigma2-plugin-extensions-opdboot',
 		author_email='ahmedmoselhi55@gmail.com',
 		package_dir={'Extensions.OPDBoot': 'src'},
 		packages=['Extensions.OPDBoot'],
-		package_data={'Extensions.OPDBoot': ['*.mvi', '*.png', 'images/*.png', 'ubi_reader/*', 'ubi_reader/ubi/*', 'ubi_reader/ubifs/*', 'ubi_reader/ubi_io/*', 'ubi_reader/ui/*', 'bin/*', 'OPDBoot_client/*', 'OPDBoot_client/images/*.png']},
+		package_data={'Extensions.OPDBoot': ['*.mvi', '*.png', 'images/*.png', 'ubi_reader/*', 'ubi_reader/ubi/*', 'ubi_reader/ubi/volume/*', 'ubi_reader/ubi/headers/*', 'ubi_reader/ubi/block/*', 'ubi_reader/ubifs/*', 'ubi_reader/ubifs/nodes/*', 'ubi_reader/ubi_io/*', 'ubi_reader/ui/*', 'bin/*', 'OPDBoot_client/*', 'OPDBoot_client/images/*.png']},
 		description='OPD MultiBoot',
 		cmdclass=setup_translate.cmdclass)
