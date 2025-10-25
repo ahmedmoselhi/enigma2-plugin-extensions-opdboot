014f399ff1283a74a4ea5b85ff9d7c02fb20c128
ahmedmoselhi <ahmedmoselhi55@gmail.com>
2025-10-25 13:19:34 +0300

    fix package data

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='enigma2-plugin-extensions-opdboot',
 		author_email='ahmedmoselhi55@gmail.com',
 		package_dir={'Extensions.OPDBoot': 'src'},
 		packages=['Extensions.OPDBoot'],
-		package_data={'Extensions.OPDBoot': ['*.mvi', '*.png', 'images/*.png']},
+		package_data={'Extensions.OPDBoot': ['*.mvi', '*.png', 'images/*.png', 'ubi_reader/*', 'OPDBoot_client/*', 'bin/*']},
 		description='OPD MultiBoot',
 		cmdclass=setup_translate.cmdclass)
